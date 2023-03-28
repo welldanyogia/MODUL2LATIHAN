924f1100e6e7f2059351bff5539f081a687257be
Welldan Yogia <63446127+welldanyogia@users.noreply.github.com>
2023-03-28 09:02:25 +0700

    w

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,16 @@
 import java.util.ArrayList;
-import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
 
-        List<String> hewan = new ArrayList<>();
+        ArrayList<String> hewan = new ArrayList<>();
         hewan.add("Sapi");
         hewan.add("Kelinci");
         hewan.add("Kambing");
         hewan.add("Unta");
         hewan.add("Domba");
 
-        List<String> deleteHewan = new ArrayList<>();
+        ArrayList<String> deleteHewan = new ArrayList<>();
         deleteHewan.add("Kelinci");
         deleteHewan.add("Kambing");
         deleteHewan.add("Unta");
